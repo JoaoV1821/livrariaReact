6aa8ce3bb18d2c209712117da4171904c9faf986
JV <joaovitoraraujo1821@gmail.com>
2022-08-04 20:23:09 -0300

    Criação do componente footer e adição da pasta assets

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
 import Home from './pages/Home/Home.jsx';
 
 const App = () => {
@@ -12,7 +13,11 @@ const App = () => {
       <Switch>
         <Route path='/' element={<Home />}></Route>
       </Switch>
+
+      <Footer/>
     </BrowserRouter>
+
+   
   )
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+         copyright 2022&copy; Grupo10 - Módulo5 - Turma EBANX - Resilia. Todos os direitos reservados
+    </footer>
+  )
+}
+
+export default Footer
